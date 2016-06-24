26ac0ad49b3bf02d35a722b78a42faf621362dd3
Tony Quintero <work@tonyquintero.com>
2016-06-24 13:06:36 -0700

    TQ - cleaning up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,14 @@ var fs = require('fs');
 var sass = require('gulp-sass');
 var mustache = require('gulp-mustache');
 
+// read the json file to get values for constants
 gulp.task('compile-css-variables', function() {
     return gulp.src('source/scss/**/*.scss')
         .pipe(mustache('source/defaults/constants.json',{},{}))
         .pipe(gulp.dest('compiled/scss'));
 });
 
+// render the sass files into css
 gulp.task('render-css', function() {
     return gulp.src(['compiled/scss/core/**/*.scss',
         'compiled/scss/components/**/*.scss'])
@@ -21,37 +23,12 @@ gulp.task('render-css', function() {
         .pipe(gulp.dest('dist/css'));
 });
 
-
-// aggregate your variables into a single object
-// gulp.task('aggregate-css-variables', function() {
-//    return gulp.src('source/defaults/**/*.json')
-//        .pipe(mustache({
-//        }))
-//        .pipe(gulp.dest('dist/css'));
-//});
-/*
-// use mustache to compile vars from json into sass
-gulp.task('insert-css-variables', function() {
-    var mustache_variables = 
-    return gulp.src('source/scss/*.scss')
-        .pipe(mustache({
-        }))
-        .pipe(gulp.dest('dist/css'));
-});
-
-// compile sass into final css
-gulp.task('sass', function() {
-    return gulp.src('source/scss/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('dist/css'));
-});
-
 // watch files for changes
 gulp.task('watch', function() {
-    gulp.watch('source/scss/*.scss', ['sass']);
+    gulp.watch('source/scss/*.scss', ['compile-css-variables', 'render-css']);
 });
 */
 // default task
-gulp.task('default', ['compile-css-variables', 'render-css']);
+gulp.task('default', ['compile-css-variables', 'render-css', 'watch']);
 
 
