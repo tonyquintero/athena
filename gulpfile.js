b9a0b921133ecf63d5d7fbf1aa81042ed6c3f529
Tony Quintero <work@tonyquintero.com>
2016-06-24 11:56:52 -0700

    TQ - digging into the meat of this now

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,31 @@ var gulp = require('gulp');
 
 // include plugins
 var sass = require('gulp-sass');
+var mustache = require('gulp-mustache');
 
-// compile sass
+gulp.task('test', function() {
+    return gulp.src('source/defaults/**/*.json')
+        .pipe(gulp.dest('dist/css'));
+});
+
+// aggregate your variables into a single object
+// gulp.task('aggregate-css-variables', function() {
+//    return gulp.src('source/defaults/**/*.json')
+//        .pipe(mustache({
+//        }))
+//        .pipe(gulp.dest('dist/css'));
+//});
+/*
+// use mustache to compile vars from json into sass
+gulp.task('insert-css-variables', function() {
+    var mustache_variables = 
+    return gulp.src('source/scss/*.scss')
+        .pipe(mustache({
+        }))
+        .pipe(gulp.dest('dist/css'));
+});
+
+// compile sass into final css
 gulp.task('sass', function() {
     return gulp.src('source/scss/*.scss')
         .pipe(sass())
@@ -13,8 +36,10 @@ gulp.task('sass', function() {
 
 // watch files for changes
 gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass']);
+    gulp.watch('source/scss/*.scss', ['sass']);
 });
-
+*/
 // default task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['test']);
+
+
